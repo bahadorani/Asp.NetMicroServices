fc18d1f43e3667cec8043a351d2f5d9d02cddfc6
bahareh.bahadorani@yahoo.com <bahareh.bahadorani@yahoo.com>
2022-05-29 00:14:39 +0430

    Set settings of Discount.Grpc Entities,Repositories,Extensions and Program class

---
+++ src/Services/Discount/Discount.Grpc/Repository/IDiscountRepository.cs
@@ -0,0 +1,16 @@
+﻿using Discount.Grpc.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Repository
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string ProductName);
+        Task<bool> CreateDiscount(Coupon copoun);
+        Task<bool> UpdateDiscount(Coupon copoun);
+        Task<bool> DeleteDiscount(string ProductName);
+    }
+}
