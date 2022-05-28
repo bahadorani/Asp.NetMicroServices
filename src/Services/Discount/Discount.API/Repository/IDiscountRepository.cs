77811ef89eb532a4a346a81912849e2910d8b5f7
bahareh.bahadorani@yahoo.com <bahareh.bahadorani@yahoo.com>
2022-05-28 13:32:55 +0430

    Discount Repository and Create CRUD  in DiscountRepository

---
+++ src/Services/Discount/Discount.API/Repository/IDiscountRepository.cs
@@ -0,0 +1,16 @@
+﻿using Discount.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repository
+{
+   public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string ProductName);
+        Task<bool> CreateDiscount(Coupon copoun);
+        Task<bool> UpdateDiscount(Coupon copoun);
+        Task<bool> DeleteDiscount(string ProductName);
+    }
+}
